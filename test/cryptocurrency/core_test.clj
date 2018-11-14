99eca2193bbe433f5f468e8ceb90867a07cfb821
fariba <fmassah@cj.com>
2018-11-14 12:51:50 -0800

    Initial commit

---
+++ test/cryptocurrency/core_test.clj
@@ -0,0 +1,7 @@
+(ns cryptocurrency.core-test
+  (:require [clojure.test :refer :all]
+            [cryptocurrency.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
