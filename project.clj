35e878996405729d01247067daf7d04f4148c3a6
fariba <fmassah@cj.com>
2018-11-21 14:05:22 -0800

    Update clojure version to 1.10 beta

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject cryptocurrency "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "fariba coins"
+  :url "none"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta1"]
                  [com.google.code.gson/gson "2.8.5"]]
   :main ^:skip-aot cryptocurrency.core
   :target-path "target/%s"
